9daba7445700f6624b8aff858f7853b86019e03c
Elias Watson <yt.assossa@gmail.com>
2018-05-23 11:45:24 -0400

    Get Game Playtime By User List

--- wilsg.py
+++ wilsg.py
@@ -4,7 +4,7 @@ import json
 import config # You need to make config.py in this directory with the API_KEY variable set to your Steam API Key
 
 review_url = "https://store.steampowered.com/appreviews/%d?json=1&review_type=positive&purchase_type=steam&start_offset=%d"
-playtime_url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%d&include_played_free_games=1&include_appinfo=1&format=json"
+playtime_url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&include_played_free_games=1&include_appinfo=1&format=json"
 
 def get_review_user_ids(app_id, amount):
     user_ids = []
@@ -15,7 +15,7 @@ def get_review_user_ids(app_id, amount):
         data = json.loads(r.text)
 
         for review in data["reviews"]:
-            user_ids.append(review["author"]["steamid"])
+            user_ids.append(review["author"]["steamid"].encode('utf-8').strip())
             if len(user_ids) == amount:
                 return user_ids
 
@@ -27,12 +27,23 @@ def get_game_playtime_user(user_id):
     r = requests.get(playtime_url % (config.API_KEY, user_id))
     data = json.loads(r.text)
 
+    if "games" not in data["response"]:
+        return {}
+
     playtime = {}
     for g in data["response"]["games"]:
-        playtime[g["name"]] = g["playtime_forever"]
+        if g["playtime_forever"] > 0:
+            playtime[g["name"].encode('utf-8').strip()] = g["playtime_forever"]
 
     return playtime
 
-playtime = get_game_playtime_user(76561198097022834)
-for i,v in enumerate(playtime):
-    print "%s - %s" % (v, playtime[v])
+def get_game_playtime_user_list(user_ids):
+    playtime = {}
+    for user_id in user_ids:
+        user_playtime = get_game_playtime_user(user_id)
+        playtime = { k: playtime.get(k, 0) + user_playtime.get(k, 0) for k in set(playtime) | set(user_playtime) }
+    return playtime
+
+playtime = get_game_playtime_user_list(get_review_user_ids(435150, 20))
+for key, value in sorted(playtime.iteritems(), key=lambda (k,v): (v,k)):
+    print "%s - %s" % (value, key)
